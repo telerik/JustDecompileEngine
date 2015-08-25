442309bd7b5afb11f17d91e417cba8a83ebbde73
mark2011 <mark2011@users.noreply.github.com>
2015-08-25 16:52:02 -0400

    .

--- appveyor-build.bat
+++ appveyor-build.bat
@@ -1,2 +1,1 @@
-"\Program Files (x86)\MSBuild\12.0\Bin\MSBuild.exe" 
-/verbosity:quiet /logger:"C:\Program Files\AppVeyor\BuildAgent\Appveyor.MSBuildLogger.dll"
+"\Program Files (x86)\MSBuild\12.0\Bin\MSBuild.exe" /verbosity:quiet /logger:"C:\Program Files\AppVeyor\BuildAgent\Appveyor.MSBuildLogger.dll"
